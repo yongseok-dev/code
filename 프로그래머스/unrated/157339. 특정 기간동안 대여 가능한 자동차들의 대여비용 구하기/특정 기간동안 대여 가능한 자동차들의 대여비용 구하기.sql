1b46cfa5cd7b4215534872a47dd1520cf709605e
yongseok <106823684+yongseok-dev@users.noreply.github.com>
2023-02-25 01:39:50 +0900

    [unrated] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/unrated/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,18 +1,22 @@
 -- 코드를 입력하세요
-SELECT crcc.car_id, crcc.car_type, 30*crcc.daily_fee*(1-crcdp.discount_rate/100) AS FEE
-FROM CAR_RENTAL_COMPANY_CAR crcc LEFT JOIN (
-    SELECT * 
-    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
-    WHERE duration_type = '30일 이상'
-) crcdp
-ON crcc.car_type=crcdp.car_type
-WHERE crcc.car_type IN('SUV','세단') AND crcc.car_id IN(
-    SELECT car_id
-    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-    GROUP BY car_id
-    HAVING MAX(CASE WHEN TO_DATE('2022-11-01','YYYY-MM-DD') BETWEEN start_date AND end_date THEN 1
-               WHEN TO_DATE('2022-11-30','YYYY-MM-DD') BETWEEN start_date AND end_date THEN 1 
-               ELSE 0 END)=0
-) AND 30*crcc.daily_fee*(1-crcdp.discount_rate/100)>=500000 AND 30*crcc.daily_fee*(1-crcdp.discount_rate/100)<2000000
-ORDER BY FEE DESC, crcc.car_type ASC, crcc.car_id DESC;
+SELECT *
+FROM(
+    SELECT crcc.car_id, crcc.car_type, 30*crcc.daily_fee*(1-crcdp.discount_rate/100) AS FEE
+    FROM CAR_RENTAL_COMPANY_CAR crcc LEFT JOIN (
+        SELECT * 
+        FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+        WHERE duration_type = '30일 이상'
+    ) crcdp
+    ON crcc.car_type=crcdp.car_type
+    WHERE crcc.car_type IN('SUV','세단') AND crcc.car_id IN(
+        SELECT car_id
+        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+        GROUP BY car_id
+        HAVING MAX(CASE WHEN TO_DATE('2022-11-01','YYYY-MM-DD') BETWEEN start_date AND end_date THEN 1
+                   WHEN TO_DATE('2022-11-30','YYYY-MM-DD') BETWEEN start_date AND end_date THEN 1 
+                   ELSE 0 END)=0
+    ) 
+)
+WHERE FEE>=500000 AND FEE<2000000
+ORDER BY FEE DESC, car_type ASC, car_id DESC;
 
