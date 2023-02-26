50bb9eac1a03ba3e78a3e1e8d1ada4c074a0eb5d
yongseok <106823684+yongseok-dev@users.noreply.github.com>
2023-02-26 21:37:43 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -4,7 +4,20 @@ FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY ccrh
     ON ccrh.car_id = ccc.car_id 
     LEFT JOIN (SELECT * FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN) crcdp
     ON (ccc.car_type = crcdp.car_type 
-        AND (end_date-start_date+1) >= REGEXP_REPLACE(duration_type, '[^0-9]+'))
+        AND (end_date-start_date+1) >= TO_NUMBER(SUBSTR(duration_type,1,INSTR(duration_type,'일')-1)))
 WHERE ccc.car_type = '트럭'
 GROUP BY history_id
-ORDER BY fee DESC, history_id DESC;
+ORDER BY fee DESC, history_id DESC
+
+
+
+-- SELECT history_id, MIN(daily_fee*(1-NVL(DISCOUNT_RATE,0)/100)*(end_date-start_date+1)) AS fee
+-- FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY ccrh 
+--     LEFT JOIN CAR_RENTAL_COMPANY_CAR ccc
+--     ON ccrh.car_id = ccc.car_id 
+--     LEFT JOIN (SELECT * FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN) crcdp
+--     ON (ccc.car_type = crcdp.car_type 
+--         AND (end_date-start_date+1) >= REGEXP_REPLACE(duration_type, '[^0-9]+'))
+-- WHERE ccc.car_type = '트럭'
+-- GROUP BY history_id
+-- ORDER BY fee DESC, history_id DESC;
