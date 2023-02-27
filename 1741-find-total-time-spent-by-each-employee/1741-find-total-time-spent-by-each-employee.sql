dde3f4dd2fcc07d10595176d4e759e3275dfeca7
yongseok <106823684+yongseok-dev@users.noreply.github.com>
2023-02-27 17:45:37 +0900

    Time: 786 ms (71.18%), Space: 0B (100.00%) - LeetHub

---
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+SELECT TO_CHAR(event_day,'YYYY-MM-DD') AS day, emp_id, SUM(out_time-in_time) AS total_time
+FROM employees
+GROUP BY TO_CHAR(event_day,'YYYY-MM-DD'), emp_id
