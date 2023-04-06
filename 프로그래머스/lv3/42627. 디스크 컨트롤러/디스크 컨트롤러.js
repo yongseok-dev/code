ba9e658b9ad4bdf580ef3f54ac76056a2a896046
yongseok <106823684+yongseok-dev@users.noreply.github.com>
2023-04-06 15:17:00 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -0,0 +1,28 @@
+function solution(jobs) {
+    var answer = 0;
+    const queue = [];
+    let point = 0;
+    let count = 0;
+    let time = 0;
+    jobs.sort((a,b) => a[0]-b[0]);
+    while(count<jobs.length){
+        let isPush = false;
+        while(point<jobs.length&&time>=jobs[point][0]){
+            queue.push(jobs[point++]);
+            isPush = true;
+        }
+        if(isPush&&queue.length>1){
+            queue.sort((a,b) => b[1]-a[1]);
+        }
+        if(queue.length>0){
+            const temp = queue.pop();
+            time += temp[1];
+            answer += (time-temp[0]);
+            count++;
+        }else{
+            time++;
+        }
+        // console.log(time,answer)
+    }
+    return Math.floor(answer/count);
+}
